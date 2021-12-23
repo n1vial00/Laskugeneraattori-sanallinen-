32272cf68fcf91d2f3870fa15a143d9c8e806953
Aleksi Viitanen <n1vial00@students.oamk.fi>
2021-12-23 14:28:48 +0200

    Neljäs pohja aloitettu

--- js/code.js
+++ js/code.js
@@ -19,15 +19,17 @@ Oraalinesteessä on penisilliiniä 100 000 IU/ml. Kuinka paljon 4-vuotiaalle 16-
 Markulle on määrätty korvatippoja. Korvatippojen vahvuus on 5 mg/ml. a) Kuinka monta millilitraa on annos, jos potilaalle on määrätty 2 mg? b) Kuinka monta tippaa (guttaa, gtt) on annos? (1ml = 20 gtt) a) Lääke 5mg - 1ml Kysytään kuinka paljon on 2 mg milliliroina? Verranto: 5 mg/ml= 2mg/X X x 5mg = 2mg x 1 ml X= 2mgx 1ml /5 mg X = 0,4 ml Vastaus: Lääkemääräys on 0,4 ml b) Kuinka monta tippaa 0,4 ml on? Tiedetään, että: 1 ml = 20 gtt 1ml/20gtt = 0.4 ml/ X X x 1 ml = 20 gtt x 0,4 ml X = 20 gtt x 0,4 ml/1 ml X = 8 gtt Vastaus: Lääkemääräys on 8 gtt
 */
 
-let EKA_POHJA = ["Lääkeliuoksen pitoisuus on ", "pitoisuus"," mg/ml. ", "nimi", " saa vuorokaudessa ", "määrä", " g painokiloa kohti ja ", " painaa ", "paino", " kg. Kuinka paljon liuosta annat kerrallaan, kun vuorokausiannos jaetaan kolmeen annostelukertaan? Ilmoita määrä millilitroina.", 0];
+const EKA_POHJA = ["Lääkeliuoksen pitoisuus on ", "pitoisuus"," mg/ml. ", "nimi", " saa vuorokaudessa ", "määrä", " g painokiloa kohti ja ", " painaa ", "paino", " kg. Kuinka paljon liuosta annat kerrallaan, kun vuorokausiannos annetaan ", "vuorokausiannosmäärä", " kertaa päivässä? Ilmoita määrä millilitroina.", 0];
 
-let TOKA_POHJA = ["Lääkeliuoksen pitoisuus on ", "pitoisuus", " mg/ml. ", "nimi", " tarvitsee vaikuttavaa ainetta ", "määrä", " g/kg kerralla. Kuinka paljon mittaat liuosta, jos potilaan paino on ", "paino", " kg? Ilmoita määrä millilitroina.", 1];
+const TOKA_POHJA = ["Lääkeliuoksen pitoisuus on ", "pitoisuus", " mg/ml. ", "nimi", " tarvitsee vaikuttavaa ainetta ", "määrä", " g/kg kerralla. Kuinka paljon mittaat liuosta, jos potilaan paino on ", "paino", " kg? Ilmoita määrä millilitroina.", 1];
 
-let KOLMAS_POHJA = ["Potilaalle annettavan lääkeliuoksen pitoisuus on ", "pitoisuus", " mg/ml. ", "nimi", " tarvitsee vaikuttavaa ainetta ", "määrä", " mg/kg/vrk. Kuinka monta millilitraa mittaat potilaalle liuosta, jos hän painaa ", "paino", " kg ja lääkettä annetaan kaksi kertaa vuorokaudessa? Ilmoita määrä millilitroina.", 2];
+const KOLMAS_POHJA = ["Potilaalle annettavan lääkeliuoksen pitoisuus on ", "pitoisuus", " mg/ml. ", "nimi", " tarvitsee vaikuttavaa ainetta ", "määrä", " mg/kg/vrk. Kuinka monta millilitraa mittaat potilaalle liuosta, jos hän painaa ", "paino", " kg ja lääkettä annetaan ", "vuorokausiannosmäärä", " kertaa vuorokaudessa? Ilmoita määrä millilitroina.", 2];
+
+const NELJ_POHJA = ["Oraalinesteessä on penisilliiniä", "pitoisuus", "IU/ml. Kuinka paljon ", "ikä", "-vuotiaalle ", "paino", "-kiloiselle Sebationille annetaan liuosta, kun penisilliiniä on määrätty ", "määrä", " IU painokiloa kohti vuorokaudessa ja lääkettä annetaan ", "vuorokausiannosmäärä", " kertaa vuorokaudessa?", 3];
 
 let POHJAT = [EKA_POHJA, TOKA_POHJA, KOLMAS_POHJA];
 
-let NIMET = ["Julma", "Elli", "Mikke", "Jusseh", "MTLVMPR", "Yoker", "Jebbe", "Otti", "Vladimir", "Cirilla", "Leo", "Tuukka", "Justus", "Lucia", "Anda", "Kuura"]
+let NIMET = ["Matias", "Elli", "Mikael", "Jussi", "Aleksi", "Julia", "Jesper", "Otso", "Vladimir", "Siri", "Leo", "Tuukka", "Justus", "Lucia", "Anda", "Kuura", "Antti", "Henna", "Tuomas", "Esko", "Kalevi", "Aada", "Lahja", "Anselmi", "Enni", "Maria", "Heimo", "Jutta", "Sanna", "Pekka", "Kasper", "Kyllikki", "Meri", "Linnea", "Kerttu", "Eerika", "Tahvo", "Mikke", "Elias", "Siiri", "Samu", "Sasu", "Iivo", "Moonika", "Mimmi", "Jesse", "Joonas", "Tuuli", "Miira", "Raija", "Martti", "Matti", "Ilmari", "Heikki", "Sirpa", "Timo", "Minna", "Riitta", "Yrjö", "Leena", "Akseli", "Aatami", "Eeva", "Mohammed", "Abdulasis", "Kasper", "Tiina", "Nea", "Oona", "John"]
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
@@ -48,14 +50,21 @@ function teeLaskut() {
         randomPaino.textContent = getRndInteger(2, 135);
         let randomAnnos = document.createElement("span");
         randomAnnos.textContent = Number(getRndInteger(1,100)/getRndInteger(100,500)).toFixed(3);
-        let randomPohja = getRndInteger(0,POHJAT.length);
-        let randomNimi = getRndInteger(0,NIMET.length);
+        let randomPohja = getRndInteger(0, POHJAT.length);
+        let randomNimi = getRndInteger(0, NIMET.length);
+        let randomAge = document.createElement("span");
+        randomAge.textContent = getRndInteger(0, 120);
         let uusiLasku = document.createElement("p");
+        let vkAnnosMr = document.createElement("span");
+        vkAnnosMr.textContent = getRndInteger(2, 5);
+        let vkamrCheck = 0;
 
         input.classList.add("vastaus");
         randomLiuosPitoisuus.classList.add("liuos")
         randomPaino.classList.add("paino");
         randomAnnos.classList.add("annos");
+        vkAnnosMr.classList.add("vkamr");
+        randomAge.classList.add("age")
 
         if(POHJAT[randomPohja][POHJAT[randomPohja].length - 1] == 2) {
             randomAnnos.textContent *= 1000;
@@ -67,7 +76,6 @@ function teeLaskut() {
                 kaavaNr.classList.add("kaava")
                 kaavaNr.textContent = POHJAT[randomPohja][j];
                 uusiLasku.append(kaavaNr);
-
             }else {
                 switch(POHJAT[randomPohja][j]) {
                     case "pitoisuus":
@@ -84,6 +92,13 @@ function teeLaskut() {
                         uusiNimi.textContent = NIMET[randomNimi];
                         uusiLasku.append(uusiNimi);
                         break;
+                    case "vuorokausiannosmäärä":
+                        uusiLasku.append(vkAnnosMr);
+                        vkamrCheck = 1;
+                        break;
+                    case "ikä":
+                        uusiLasku.append(randomAge);
+                        break;
                     default:
                         let pohjPal = document.createElement("span");
                         pohjPal.textContent = POHJAT[randomPohja][j];
@@ -92,6 +107,10 @@ function teeLaskut() {
                 }
             }
 
+        }
+        if(vkamrCheck == 0) {
+            vkAnnosMr.textContent = "";
+            uusiLasku.append(vkAnnosMr);
         }
         div.append(uusiLasku, input);
         kaikkiLaskut.append(div);
@@ -106,18 +125,19 @@ function tarkista() {
     let liuokset = document.querySelectorAll(".liuos");
     let annokset = document.querySelectorAll(".annos");
     let kaavat = document.querySelectorAll(".kaava");
+    let vkAnnosMr = document.querySelectorAll(".vkamr");
 
     for(i = 0; i < vastaukset.length; i++){
         let vastaus = 0;
         switch(Number(kaavat[i].textContent)) {
             case 0:
-                vastaus = Number((((annokset[i].textContent * painot[i].textContent) / 3 /* <-- Tän vois kans vaihtaa muuttujaksi */) * 1000) * (1 / liuokset[i].textContent)).toFixed(1);
+                vastaus = Number((((annokset[i].textContent * painot[i].textContent) / vkAnnosMr[i].textContent) * 1000) * (1 / liuokset[i].textContent)).toFixed(1);
                 break;
             case 1:
                 vastaus = Number((annokset[i].textContent * painot[i].textContent * 1000) * (1 / liuokset[i].textContent)).toFixed(1);
                 break;
             case 2:
-                vastaus = Number((((annokset[i].textContent * painot[i].textContent)) / 2) * (1 / liuokset[i].textContent)).toFixed(1);
+                vastaus = Number((((annokset[i].textContent * painot[i].textContent)) / vkAnnosMr[i].textContent) * (1 / liuokset[i].textContent)).toFixed(1);
                 break;
         }
         if(Number(vastaukset[i].value) == Number(vastaus)) {
